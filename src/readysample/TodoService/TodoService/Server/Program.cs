4bfc58446a124f3a100e3fed42d18163675f8e0a
LeeJonghoon <jonghoon.1738@gmail.com>
2023-08-22 23:04:54 +0900

    발표용 소스 완료

--- src/readysample/TodoService/TodoService/Server/Program.cs
+++ src/readysample/TodoService/TodoService/Server/Program.cs
@@ -10,31 +10,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-#region 데이터베이스컨텍스트 설정부분  
-var connection = builder.Configuration.GetConnectionString("Default");  
-builder.Services.AddDbContext<TodoService.Server.Database.Context.TodoServiceContext>(option =>  
-{  
-    option.UseMySql(connection, ServerVersion.AutoDetect(connection));  
-});  
-
-#endregion
-
-#region Swagger 생성기 주입
-builder.Services.AddSwaggerGen(c =>  
-{  
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });  
-});  
+#region db&swagger DI
+// #region 데이터베이스컨텍스트 설정부분  
+// var connection = builder.Configuration.GetConnectionString("Default");  
+// builder.Services.AddDbContext<TodoService.Server.Database.Context.TodoServiceContext>(option =>  
+// {  
+//     option.UseMySql(connection, ServerVersion.AutoDetect(connection));  
+// });  
+//
+// #endregion
+//
+// #region Swagger 생성기 주입
+// builder.Services.AddSwaggerGen(c =>  
+// {  
+//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });  
+// });  
+// #endregion
 #endregion
   
 var app = builder.Build();  
 
 #region 스웨거 사용설정
-app.UseSwagger();  
-app.UseSwaggerUI(x =>  
-{  
-    x.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");  
-  
-});
+// app.UseSwagger();  
+// app.UseSwaggerUI(x =>  
+// {  
+//     x.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");  
+//   
+// });
 #endregion
 
 if (app.Environment.IsDevelopment())
@@ -44,7 +46,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
